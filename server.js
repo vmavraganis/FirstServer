535bbefaa2fefe361f65a2dfe17b12fdc3d87d78
vmavraganis <vlmavraganis@hotmail.com>
2017-03-16 17:13:22 +0200

    16/3/2017 17:15
    added partials

--- server.js
+++ server.js
@@ -2,26 +2,38 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 
-var app = express();
+var app = express(); 
 
+hbs.registerPartials(`${__dirname}/views/partials`);
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (request, response) => {
     response.render('home.hbs',
         {
             user: process.env['USERPROFILE'].split(path.sep)[2],
             pageTitle: "Home Page",
-            message:function() {return `Welcome to my website ${this.user}`;},
-            currentYear: new Date().getFullYear()
+            message: function () { return `Welcome to my website ${this.user}`; },
+            
         });
 });
 app.get('/about', (request, response) => {
     response.render('about.hbs',
         {
-            title: "About page",
-            currentYear: new Date().getFullYear()
+            pageTitle: "About page",
+            message:"Where to we go from here futureeeee wooooorld"
         });
+})
+
+app.get('/help', (request, response) => {
+    response.render('help.hbs', {
+
+       pageTitle: "Help page",
+        message:"We are here to help you"
+    });
 })
 app.get('/bad', (request, response) => {
     response.send({
