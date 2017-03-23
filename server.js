42a377f8d23cee02e338e16c46c20d709eac6619
vmavraganis <vlmavraganis@hotmail.com>
2017-03-23 13:30:03 +0200

    23.3.2017 13:29
    Server.js improvments

--- server.js
+++ server.js
@@ -1,30 +1,64 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
-
+const fs=require('fs');
 var app = express(); 
 
 hbs.registerPartials(`${__dirname}/views/partials`);
+app.set('view engine', 'hbs');
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    let log=`${now} :${req.method} : ${req.url}`
+    console.log(log);
+    fs.appendFile("server.log",log+`\n`,(err)=>{
+        if(err){
+        console.log('Unable to append to server.log')};
+    })
+    next();
+})
+
+// app.use ((req,res,next)=>{
+//     res.render("maintenance.hbs");
+// });
+
+app.use(express.static(`${__dirname}/public`));
+
 hbs.registerHelper('getCurrentYear',()=>{
     return new Date().getFullYear();
-})
-app.set('view engine', 'hbs');
-app.use(express.static(`${__dirname}/public`));
+});
+hbs.registerHelper('screamIt', (text) => {
+ return text.toUpperCase();
+});
+
+
+
+
+
 
 app.get('/', (request, response) => {
     response.render('home.hbs',
         {
             user: process.env['USERPROFILE'].split(path.sep)[2],
             pageTitle: "Home Page",
-            message: function () { return `Welcome to my website ${this.user}`; },
-            
+            message:"welcome to my site"
+            //message: function () { return `Welcome to my website ${this.user}`; },
+        
         });
 });
 app.get('/about', (request, response) => {
-    response.render('about.hbs',
+    response.render('about.hbs', 
         {
             pageTitle: "About page",
             message:"Where to we go from here futureeeee wooooorld"
+        });
+})
+  
+app.get('/projects', (request, response) => {
+    response.render('about.hbs', 
+        {
+            pageTitle: "Project page",
+            message:"Here are my Projects"
         });
 })
 
